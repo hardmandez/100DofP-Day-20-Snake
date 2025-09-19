3a73fa9be8bb626ca169f81b0973605f86458b24
joseh <hardmandez@hotmail.com>
2025-09-19 20:11:26 +0100

    190925 2011

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 import time
 from turtle import Screen, Turtle
-
+from snake import Snake
 screen = Screen()
 screen.setup(width=600, height=600)
 screen.bgcolor("black")
@@ -8,29 +8,23 @@ screen.title("Snake Game")
 screen.tracer(0)
 
 
-#Define new_snake_segment
+my_snake = Snake()
+screen.update()
 
 
-# def move_snake_left():
-#     segment[0].left(90)
+
+# print(snake_position)
+# game_is_on = True
+# while game_is_on:
+#     screen.update()
+#     time.sleep(0.2)
 #
-# def move_snake_right():
-#     segment[0].right(90)
-
-
-
-print(snake_position)
-game_is_on = True
-while game_is_on:
-    screen.update()
-    time.sleep(0.2)
-
-    for seg_no in range(len(segment) - 1, 0, -1):
-        new_x = segment[seg_no - 1].xcor()
-        new_y = segment[seg_no - 1].ycor()
-        segment[seg_no].goto(new_x, new_y)
-
-    segment[0].forward(20)
+#     for seg_no in range(len(segment) - 1, 0, -1):
+#         new_x = segment[seg_no - 1].xcor()
+#         new_y = segment[seg_no - 1].ycor()
+#         segment[seg_no].goto(new_x, new_y)
+#
+#     segment[0].forward(20)
 
     # screen.onkey(move_snake_left,"o")
     # screen.onkey(move_snake_right,"p")
--- snake.py
+++ snake.py
@@ -1,20 +1,23 @@
 from turtle import Screen, Turtle
 
-def ___init___():
-    snake_length = 5
-    snake_start_x = 0
-    snake_start_y = 0
-    segment = []
-    snake_position = []
+class Snake():
+    def __init__(self):
+        self.snake_length = 5
+        self.snake_start_x = 0
+        self.snake_start_y = 0
+        self.segment = []
+        self.snake_position = []
+        self.createsnake()
 
+    def createsnake(self):
+        for _ in range(self.snake_length):
+            self.new_snake_segment = Turtle(shape="square")
+            self.new_snake_segment.penup()
+            self.new_snake_segment.color("white")
+            self.new_snake_segment.goto(self.snake_start_x, self.snake_start_y)
+            self.segment.append(self.new_snake_segment)
+            self.snake_position.append((self.snake_start_x, self.snake_start_y))
+            self.snake_start_x -= 20
 
-def move():
-    for _ in range(snake_length):
-        new_snake_segment = Turtle(shape="square")
-        new_snake_segment.penup()
-        new_snake_segment.color("white")
-        # new_snake_segment.speed("fastest")
-        new_snake_segment.goto(snake_start_x, snake_start_y)
-        segment.append(new_snake_segment)
-        snake_position.append((self.snake_start_x, self.snake_start_x))
-        snake_start_x -= 20
+    def MoveSnake(self):
+       return
